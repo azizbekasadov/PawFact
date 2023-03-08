93eea8c47053cd3351223e5cffdb34cdac04e30c
Azizbek Asadov <azizbek1998@icloud.com>
2023-03-08 13:50:44 +0500

    Initial Commit

---
+++ PawFact/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PawFact
+//
+//  Created by Azizbek Asadov on 08/03/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
